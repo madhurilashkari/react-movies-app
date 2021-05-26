cf2c2903c145c71eb71dcba90c603da45d227dc6
Madhuri <madhukkl2000@gmail.com>
2021-05-26 14:40:19 +0530

    Added required validation forpassword

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -9,6 +9,8 @@ import Tab from '@material-ui/core/Tab';
 import FormControl from '@material-ui/core/FormControl';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
+import PropTypes from 'prop-types';
+import FormHelperText from '@material-ui/core/FormHelperText';
 
 
 const customStyles = {
@@ -23,22 +25,34 @@ const customStyles = {
 };
 const TabContainer = function (props) {
     return (
-        <Typography component="div" style={{ padding: 0 ,textAlign :'center'}}>
+        <Typography component="div" style={{ padding: 0, textAlign: 'center' }}>
             {props.children}
         </Typography>
     );
 }
+TabContainer.propTypes = {
+    children: PropTypes.node.isRequired
+}
+
 class Header extends Component {
     constructor() {
         super();
-      
+
         this.state = {
             modelIsOpen: false,
-            value: 0
+            value: 0,
+            usernameRequired: "dispNone",
+            username: "",
+            passwordRequired :"dispNone",
+            password : ""
         }
     }
     openModelHandler = () => {
-        this.setState({ modelIsOpen: true })
+        this.setState({ modelIsOpen: true });
+      this.setState({usernameRequired :"dispNone"}) ;
+        this.setState({ value :0});
+        this.setState({ passwordRequired: "dispNone"});
+        this.setState({password: ""});
     }
 
     closeModelHandler = () => {
@@ -49,7 +63,18 @@ class Header extends Component {
         this.setState({ value })
     }
 
-   
+    loginClickHandler =() =>{
+        this.state.username === "" ? this.setState({usernameRequired :"dispBlock"}) : this.setState({usernameRequired :"dispNone"});
+        this.state.password === "" ? this.setState({passwordRequired :"dispBlock"}) : this.setState({passwordRequire :"dispNone"});
+    }
+
+    inputUsernameChangeHandler = (e) => {
+        this.setState({ username: e.target.value });
+    }
+
+    inputPasswordChangeHandler = (e) => {
+        this.setState({ password: e.target.value });
+    }
     render() {
         return (
             <div>
@@ -61,24 +86,30 @@ class Header extends Component {
                     </Button>
                     </div>
                 </header>
-                <Model ariaHideApp={false} isOpen={this.state.modelIsOpen} contentLabel="Login" onRequestClose={this.closeModelHandler}  >
+                <Model ariaHideApp={false} isOpen={this.state.modelIsOpen} contentLabel="Login" onRequestClose={this.closeModelHandler} style={customStyles} >
 
-                    <Tabs className ="tabs" value={this.state.value} centered onChange={this.tabChangeHandler}>
+                    <Tabs className="tabs" value={this.state.value} centered onChange={this.tabChangeHandler}>
                         <Tab label='Login' />
                         <Tab label='Register' />
                     </Tabs>
-                    <TabContainer >
-                        <FormControl required>
-                            <InputLabel htmlFor='userName' >User Name</InputLabel><br />
-                            <Input id ='userName' type ='text' />
-                            
-                        </FormControl><br/><br />
-                        <FormControl required>
-                        <InputLabel htmlFor='password' >Password</InputLabel>
-                            <Input id ='password' type ='password' />
-                        </FormControl><br/><br/>    
-                        <Button variant ='contained' color='primary'>Login</Button>
-                    </TabContainer>
+                    {this.state.value === 0 &&
+                        <TabContainer >
+                            <FormControl required>
+                                <InputLabel htmlFor='userName' >User Name</InputLabel><br />
+                                <Input id='userName' type='text' username={this.state.username}  onChange={this.inputUsernameChangeHandler} />
+                                <FormHelperText className={this.state.usernameRequired}>
+                                    <span className='red'>required</span>
+                                </FormHelperText>
+                            </FormControl><br /><br />
+                            <FormControl required>
+                                <InputLabel htmlFor='password' >Password</InputLabel>
+                                <Input id='password' type='password'  password={this.state.password}  onChange={this.inputPasswordChangeHandler} />
+                                <FormHelperText className={this.state.passwordRequired}>
+                                    <span className='red'>required</span>
+                                 </FormHelperText>
+                            </FormControl><br /><br />
+                            <Button variant='contained' color='primary' onClick ={this.loginClickHandler}>Login</Button>
+                        </TabContainer>}
                 </Model>
             </div>
         )
