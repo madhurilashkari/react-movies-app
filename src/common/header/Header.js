ae2c885c7c1308713ac8fe601b86df169b81d154
Madhuri <madhukkl2000@gmail.com>
2021-05-26 11:00:05 +0530

    Added username and password inside Login form

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,14 +1,37 @@
 import React, { Component } from "react"
 import "./Header.css";
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import logo from "../../assets/logo.svg";
 import Model from 'react-modal';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
 
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+};
+const TabContainer = function (props) {
+    return (
+        <Typography component="div" style={{ padding: 0 }}>
+            {props.children}
+        </Typography>
+    );
+}
 class Header extends Component {
     constructor() {
         super();
+      
         this.state = {
             modelIsOpen: false,
             value: 0
@@ -22,9 +45,11 @@ class Header extends Component {
         this.setState({ modelIsOpen: false })
     }
 
-    tabChangeHandler =(event ,value) =>{
-        this.setState({value})
+    tabChangeHandler = (event, value) => {
+        this.setState({ value })
     }
+
+   
     render() {
         return (
             <div>
@@ -36,11 +61,23 @@ class Header extends Component {
                     </Button>
                     </div>
                 </header>
-                <Model ariaHideApp={false} isOpen={this.state.modelIsOpen} contentLabel="Login" onRequestClose={this.closeModelHandler}>
-                    <Tabs value={this.state.value} onChange ={this.tabChangeHandler}>
+                <Model ariaHideApp={false} isOpen={this.state.modelIsOpen} contentLabel="Login" onRequestClose={this.closeModelHandler}  style={this.customStyles}>
+
+                    <Tabs value={this.state.value} centered onChange={this.tabChangeHandler}>
                         <Tab label='Login' />
                         <Tab label='Register' />
                     </Tabs>
+                    <TabContainer >
+                        <FormControl required>
+                            <InputLabel htmlFor='userName' >User Name</InputLabel>
+                            <Input id ='userName' type ='text' />
+                            
+                        </FormControl>
+                        <FormControl required>
+                        <InputLabel htmlFor='password' >Password</InputLabel>
+                            <Input id ='password' type ='password' />
+                        </FormControl>
+                    </TabContainer>
                 </Model>
             </div>
         )
