73f7dda0a7ab34a55f67be5f63fa90ab1583a259
Madhuri <madhukkl2000@gmail.com>
2021-05-27 21:02:46 +0530

    Added poster, title and genres on details page

---
+++ src/screens/details/Details.js
@@ -0,0 +1,59 @@
+import { Typography } from "@material-ui/core";
+import React, { Component } from "react"
+import Header from "../../common/header/Header"
+import moviesData from "../../common/movieData";
+import './Details.css';
+
+class Details extends Component {
+    constructor() {
+        super();
+        this.state = {
+            movie: {}
+        }
+    }
+    componentWillMount() {
+        let currentState = this.state;
+        currentState.movie = moviesData.filter((mov) => {
+            return mov.id === this.props.movieId
+        })[0];
+        this.setState({ currentState });
+        console.log(this.state);
+    }
+    render() {
+        let movie = this.state.movie;
+        return (
+            <div>
+                <Header />
+                <div className="flex-containerDetails">
+                    <div className="leftDetails">
+                        <img scr={movie.poster_url} alt={movie.title} />
+                    </div>
+                    <div className="middleDetails">
+                    <div>
+                            <Typography variant ='headline' component ='h2'>{movie.title } </Typography>
+                        </div>
+                        <div>
+                            <Typography><span className='bold'>Genre: </span>{movie.genres.join(', ')} </Typography>
+                        </div>
+                        <div>
+                            <Typography><span className='bold'>Duration: </span>{movie.duration} </Typography>
+                        </div>
+                        <div>
+                            <Typography><span className='bold'>Release Date: </span>{new Date(movie.release_date).toDateString()} </Typography>
+                        </div>
+                        <div>
+                            <Typography><span className='bold'>Rating: </span>{movie.rating} </Typography>
+                        </div>
+                        <div>
+                            <Typography><span className='bold'>Plot: </span><a href={movie.wiki_url}>(Wiki Link)</a>{movie.storyline} </Typography>
+                        </div>
+                    </div>
+                    <div className="rightDetails">
+
+                    </div>
+                </div>
+            </div>);
+    }
+}
+
+export default Details
